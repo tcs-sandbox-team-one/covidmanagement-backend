60ba32b8e654fbbe64edce903b7a9f6d0794960a
santhosh <santhoshramanathan25@gmail.com>
2020-08-05 11:37:21 +0530

    updated get

--- app/server.js
+++ app/server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const cors = require("cors");
-const port = 4000;
+ const port = 8080; 
 app.use(cors());
 
 const server = http.createServer(app);
 
 app.get("/test", (req, res) => {
   res.status(200).send("Server is running...");
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running at a great speed...");
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
